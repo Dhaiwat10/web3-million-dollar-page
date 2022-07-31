f9c2b972999d7b5ccfdea774854b0394c9242483
crypdough.eth <106392730+crypdoughdoteth@users.noreply.github.com>
2022-07-31 10:00:24 -0400

    Update MillionDollarNFT.sol

--- packages/hardhat/contracts/MillionDollarNFT.sol
+++ packages/hardhat/contracts/MillionDollarNFT.sol
@@ -4,129 +4,101 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ERC4907.sol";
 
-contract MillionDollarNFT is ERC4907, Ownable {
+    
+contract millionDollarHomepageNFT is ERC4907, Ownable{
     error expired();
-    mapping(uint256 => string) internal _idToURI;
-    uint256 internal index;
+    mapping (uint => string) internal _idToURI;
+    uint expiryInit;
 
-    constructor(string memory name, string memory symbol)
-        ERC4907(name, symbol)
-    {
-        //stuff here
+    constructor (string memory name, string memory symbol) ERC4907(name, symbol) {
+        expiryInit = 1 years; 
     }
 
-    function setUser(
-        uint256 tokenId,
-        address user,
-        uint64 expires
-    ) public override {
-        require(
-            msg.sender == owner() || _isApprovedOrOwner(msg.sender, tokenId),
-            "ERC4907: transfer caller is not owner nor approved"
-        );
-        UserInfo storage info = _users[tokenId];
-        uint64 expiry = _users[tokenId].expires;
-
-        if (msg.sender == owner()) {
-            info.user = user;
-            info.expires = expiry;
-            emit UpdateUser(tokenId, user, expiry);
-        } else if (msg.sender != owner() && block.timestamp < expiry) {
-            info.user = user;
-            info.expires = expires;
-            emit UpdateUser(tokenId, user, expires);
-        } else {
-            revert expired();
+    function setUser(uint256 tokenId, address user, uint64 expires) public override{
+        require(msg.sender == owner() || _isApprovedOrOwner(msg.sender, tokenId), "ERC4907: transfer caller is not owner nor approved");
+        uint64 expiry = _users[tokenId].expires; 
+        if (msg.sender == owner()){
+            registerUser(tokenId, user, expires);
         }
+        else if(msg.sender != owner() && block.timestamp < expiry){
+            registerUser(tokenId, user, expiry);
+        } 
+       else{
+            revert expired();
+       }
     }
 
-    function userOf(uint256 tokenId) public view override returns (address) {
-        if (uint256(_users[tokenId].expires) >= block.timestamp) {
-            return _users[tokenId].user;
-        } else {
+    function registerUser (uint tokenId, address user, uint expires) internal {
+        UserInfo storage info =  _users[tokenId];
+        info.user = user;
+        info.expires = expires;
+        emit UpdateUser(tokenId, user, expires);
+    }
+
+
+    function userOf(uint256 tokenId) public view override returns(address){
+        if( uint256(_users[tokenId].expires) >=  block.timestamp){
+            return  _users[tokenId].user;
+        }
+        else{
             return address(0);
         }
     }
 
-    function userExpires(uint256 tokenId)
-        public
-        view
-        override
-        returns (uint256)
-    {
+    function userExpires(uint256 tokenId) public view override returns(uint256){
         return _users[tokenId].expires;
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        virtual
-        override
-        returns (bool)
-    {
-        return
-            interfaceId == type(IERC4907).interfaceId ||
-            super.supportsInterface(interfaceId);
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return interfaceId == type(IERC4907).interfaceId || super.supportsInterface(interfaceId);
     }
 
-    function mint(address to, uint256 quantity) external {
-        require(msg.sender == tx.origin, "no contracts allowed");
-        for (uint256 i; i <= quantity; i++) {
-            ++index;
-            _mint(to, index);
+    function mint (address to, uint tokenId) external {
+        require (msg.sender == tx.origin, "no contracts allowed");
+        _mint(to, tokenId);
+        registerUser(tokenId, to, expiryInit);
         }
     }
 
     // @param newURI: <baseURI, points to decentralized storage>
-    function setBaseURI(string memory newURI, uint256 tokenId) public {
+     function setBaseURI (string memory newURI, uint tokenId) public {
         require(_isApprovedOrOwner(msg.sender, tokenId));
         _idToURI[tokenId] = newURI;
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        virtual
-        override
-        returns (string memory)
-    {
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         _requireMinted(tokenId);
         string memory baseURI = _baseURI(tokenId);
-        return
-            bytes(baseURI).length > 0
-                ? string(abi.encodePacked(baseURI, tokenId, ".json"))
-                : "";
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId, ".json")) : "";
     }
-
     /**
      * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
      * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
      * by default, can be overridden in child contracts.
      */
-    function _baseURI(uint256 tokenId)
-        internal
-        view
-        virtual
-        returns (string memory)
-    {
+    function _baseURI(uint tokenId) internal view virtual returns (string memory) {
         return _idToURI[tokenId];
     }
-
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 tokenId
-    ) internal virtual override {
+    ) internal virtual override{
         super._beforeTokenTransfer(from, to, tokenId);
-        uint64 expiry = _users[tokenId].expires;
+        uint64 expiry = _users[tokenId].expires; 
         delete _users[tokenId];
         //take over lease
-        if (msg.sender != owner() && block.timestamp < expiry) {
-            setUser(tokenId, to, expiry);
-        } else if (msg.sender != owner() && block.timestamp >= expiry) {
-            revert expired();
-        } else {
+        if(msg.sender == owner()){
             emit UpdateUser(tokenId, address(0), 0);
+
+        }
+        else if (msg.sender != owner() && block.timestamp < expiry){
+            setUser(tokenId, to, expiry); 
+        }
+        else{
+            revert expired(); 
         }
     }
 }
+
+
